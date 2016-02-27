f3d1a57df547fcaff83239e9406722698714f6dc
JacobGood1 <jacobegoodson@gmail.com>
2016-02-26 21:54:01 -0500

    lots of init

--- package.lisp
+++ package.lisp
@@ -1,9 +1,22 @@
 ;;;; package.lisp
 
 (defpackage :sdl
-  (:use #:cl :utilities))
+  (:use #:cl :utilities)
+  (:export :create-window
+  		   :get-window-surface
+           :fill-rect
+           :update-window-surface
+           :hide-window
+           :show-window
+           :poll-event
+           :get-event-type
+           :handle-events
+           :create-renderer
+		   :set-render-draw-color
+		   :render-clear
+		   :render-present))
 
 ;main package contains the # all others should not have it
 (defpackage #:sdl-game
-  (:use #:cl))
+  (:use #:cl :sdl))
 
---
+++ sdl-game.lisp~
@@ -0,0 +1,6 @@
+;;;; sdl-game.lisp
+
+(in-package #:sdl-game)
+
+;;; "sdl-game" goes here. Hacks and glory await!
+
